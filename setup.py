2567f9e0c74a084c06132f9865f4c27a30a7995b
Cerberus <cerberus1220@gmail.com>
2015-04-14 14:05:24 +0800

    Add python version check in install script

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
+
 from setuptools import setup, find_packages
 import platform
+import sys
 
 from fluxclient import VERSION as _VERSION
+
+if not sys.version_info >= (3, 3):
+    print("ERROR: fluxclient require Python version grather then 3.3\n")
+    sys.exit(1)
 
 VERSION = ".".join([str(i) for i in _VERSION])
 
