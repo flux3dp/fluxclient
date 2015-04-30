910befc94a3a818c32d0990ccfc42fa8badf8c13
Cerberus <cerberus1220@gmail.com>
2015-04-30 13:52:56 +0800

    Add robot function

---
+++ fluxclient/console.py
@@ -0,0 +1,63 @@
+
+import curses
+import sys
+
+class Console(object):
+    def __enter__(self):
+        self.stdscr = curses.initscr()
+        return self
+
+    def __exit__(self, type, value, tb):
+        curses.endwin()
+
+    def setup(self):
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+
+        curses.cbreak()
+        curses.echo()
+
+        lines, cols = self.stdscr.getmaxyx()
+        self.logwin = self.stdscr.subwin(lines - 2, cols, 0, 0)
+        self.sepwin = self.stdscr.subwin(1, cols, lines - 2, 0)
+        self.cmdwin = self.stdscr.subwin(1, cols, lines - 1, 0)
+
+        self.logwin.scrollok(True)
+
+        self.sepwin.bkgd(curses.color_pair(1))
+        self.sepwin.refresh()
+
+    def append_log(self, buf):
+        self.logwin.addstr(buf + "\n")
+        self.logwin.refresh()
+
+    def read_cmd(self):
+        lines, cols = self.stdscr.getmaxyx()
+        cmd = self.stdscr.getstr(lines - 1, 0).decode("utf8")
+        self.cmdwin.addstr(b" " * len(cmd) + b"\r")
+        self.cmdwin.refresh()
+
+        self.append_log("> %s" % cmd)
+        return cmd
+
+if __name__ == "__main__":
+    def dadada(iv):
+        from time import sleep
+        while True:
+            iv.logwin.addstr("dadada\n")
+            iv.logwin.refresh()
+            sleep(1.0)
+
+    with Interactive() as i:
+        i.setup()
+
+        import threading
+        t = threading.Thread(target=dadada, args = (i, ))
+        t.setDaemon(True)
+        t.start()
+
+        while True:
+            lines, cols = i.stdscr.getmaxyx()
+            buf = i.stdscr.getstr(lines - 1, 0)
+            i.append_log(buf)
+
---
+++ fluxclient/robot.py
@@ -0,0 +1,29 @@
+
+import socket
+import curses
+
+from fluxclient import encryptor as E
+
+
+class RobotClient(object):
+    def __init__(self, ipaddr=None, server_key=None, stdout=None):
+        self.sock = s = socket.socket()
+        self.output = stdout
+
+        s.connect(ipaddr)
+        buf = s.recv(4096)
+
+        ver, sign, randbytes = buf[:8], buf[8:-128], buf[-128:]
+        self.output("Warning: can not validate remote")
+
+        rsakey = E.get_or_create_keyobj()
+        self.output("Access ID: %s" % E.get_access_id(rsakey))
+        buf = E.get_access_id(rsakey, binary=True) + E.sign(rsakey, randbytes)
+        s.send(buf)
+
+        status = s.recv(16).rstrip(b"\x00").decode()
+        self.output("Handshake: %s" % status)
+
+        if status != "OK":
+            raise RuntimeError("Handshake failed.")
+
