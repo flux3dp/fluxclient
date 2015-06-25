c4b1697b92f00119b4bbbb274c750096f6a441be
Yen <yen@flux3dp.com>
2015-06-25 16:00:11 +0800

    class pc_process

---
+++ fluxclient/scanner/pc_process.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+import struct
+from operator import ge, le
+
+
+import fluxclient.scanner.scan_settings as scan_settings
+import fluxclient.scanner._scanner as _scanner
+
+
+class pc_process():
+    """process point cloud"""
+    def __init__(self):
+        self.clouds = {}  # clouds that hold all the point cloud data, key:name, value:point cloud
+
+    def upload(self, name, buffer_pc_L, buffer_pc_R, L_len, R_len):
+        self.clouds[name] = (self.unpack_data(buffer_pc_L), self.unpack_data(buffer_pc_R))
+        # upload [name] [point count L] [point count R]
+
+    def unpack_data(self, buffer_data):
+        assert len(buffer_data) % 24 == 0, "wrong buffer size %d (can't devide by 24)" % (len(buffer_data) % 24)
+        data = []
+        for p in range(len(buffer_data) / 24):
+            data.append(list(struct.unpack('<ffffff', buffer_data[p * 24:p * 24 + 24])))
+        return data
+
+    def base(self, name):
+        self.current_name = name
+
+    def cut(self, name_in, name_out, mode, direction, value):
+        """
+            manually cut the point cloud
+            mode = 'x', 'y', 'z' ,'r'
+            direction = True(>=), False(<=)
+            [FUTURE WORK] transplant to cpp in future to spped up
+        """
+        pc = self.clouds[name_in]
+        cropped_pc = []
+
+        if direction:  # ge = >=, le = <=
+            cmp_function = ge
+        else:
+            cmp_function = le
+
+        if mode == 'r':
+            for p in pc:
+                if cmp_function(p[0] ** 2 + p[1] ** 2, thres ** 2):
+                    cropped_pc.append(p)
+            return
+
+        elif mode == 'x':
+            index = 0
+        elif mode == 'y':
+            index = 1
+        elif mode == 'z':
+            index = 2
+        for p in pc:
+            if cmp_function(p[index], thres):
+                cropped_pc.append(p)
+
+        self.clouds[name_out] = cropped_pc
+
+    def to_cpp(pc_python):
+        """
+        convert python style pc into cpp style pc object
+        """
+        pc = _scanner.PointCloudXYZRGBObj()
+        for i in pc_python:
+            _scanner.push_backPoint(pc, i[0], i[1], i[2], i[3] | (i[4] << 8) | (i[5] << 16))
+        return pc
+
+    def noise_del(self, pc_source):
+        """
+        delete noise base on idstance of each point
+        pc_source could be a string indcating the file that we want or a pc object
+        """
+        if type(pc_source) == str:
+            pc = _scanner.PointCloudXYZRGBObj()
+            pc.load(pc_source)
+        else:
+            pc = pc_source
+        pc.SOR(50, 0.3)
+        return pc
+
+    def to_mesh(self):
+        pass
