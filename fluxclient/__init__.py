b979f9d8d86c9830b22889346f1130fb0cf9b5db
Cerberus <cerberus1220@gmail.com>
2015-07-13 18:23:29 +0800

    Add support const

--- fluxclient/__init__.py
+++ fluxclient/__init__.py
@@ -1,2 +1,12 @@
 
+def check_pcl():
+    try:
+        from fluxclient.scanner import _scaner
+        return True
+    except ImportError:
+        return False
+
+
 VERSION = ("0", "4a4")
+
+SUPPORT_PCL = check_pcl
