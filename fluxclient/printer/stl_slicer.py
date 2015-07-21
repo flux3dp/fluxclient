d482db71fd08d9df007ae3972843b0b43aacb6ce
Yen <yen@flux3dp.com>
2015-07-21 17:00:22 +0800

    stl reader

--- fluxclient/printer/stl_slicer.py
+++ fluxclient/printer/stl_slicer.py
@@ -1,8 +1,53 @@
 # !/usr/bin/env python3
+import struct
+
 try:
     import fluxclient.scanner._printer as _printer
 except:
     pass
+
+
+def read_stl(file_data):
+    # https://en.wikipedia.org/wiki/STL_(file_format)
+    if type(file_data) == str:
+        with open(file_data, 'rb') as f:
+            file_data = f.read()
+            Byte_Order = '@'
+    elif type(file_data) == bytes:
+        Byte_Order = '<'
+    else:
+        raise ValueError('wrong stl data type:%s' % str(type(file_data)))
+    if file_data.startswith(b'solid '):
+        # ascii stl file
+        pass
+    else:
+        # binary stl file
+        header = file_data[:80]
+        length = struct.unpack(Byte_Order + 'I', file_data[80:84])[0]
+
+        points = {}  # key: points, value: index
+        faces = []
+        counter = 0
+        for i in range(length):
+            index = i * 50 + 84
+            # n = struct.unpack(Byte_Order + 'III', file_data[index + (4 * 3 * 0):index + (4 * 3 * 1)])
+            v0 = struct.unpack(Byte_Order + 'fff', file_data[index + (4 * 3 * 1):index + (4 * 3 * 2)])
+            v1 = struct.unpack(Byte_Order + 'fff', file_data[index + (4 * 3 * 2):index + (4 * 3 * 3)])
+            v2 = struct.unpack(Byte_Order + 'fff', file_data[index + (4 * 3 * 3):index + (4 * 3 * 4)])
+            # v = struct.unpack(Byte_Order + 'I' * 9, file_data[index + (4 * 3 * 1):index + (4 * 3 * 4)])
+            face = []
+            for v in [v0, v1, v2]:
+                if v not in points:
+                    counter
+                    points[v] = counter
+                    points[counter] = v
+                    counter += 1
+                face.append(points[v])
+            faces.append(face)
+        points_list = []
+        for i in range(counter):
+            points_list.append(points[i])
+    return points_list, faces
 
 
 class StlSlicer(object):
@@ -26,6 +71,7 @@ class StlSlicer(object):
         ## self.mesh = Mesh(pcl mesh)
 
         for i in names:
+            points, faces = read_stl(buf)
             m_mesh = _printer.MeshObj()
             ## self.mesh.add_on(names)
             ## in add on, do the moving and rotating
