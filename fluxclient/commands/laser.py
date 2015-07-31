52726dcdfe0eb86040e28ea30741c700eac8c337
Yen <yen@flux3dp.com>
2015-07-31 14:56:29 +0800

    flux_laser: fix bug forget to change type

--- fluxclient/commands/laser.py
+++ fluxclient/commands/laser.py
@@ -19,6 +19,7 @@ def process_svg(options, stream):
         if arg.startswith('@'):
             try:
                 w, h, x1, y1, x2, y2, rotation, filename = arg[1:].split(",")
+                w, h, x1, y1, x2, y2, rotation = float(w), float(h), float(x1), float(y1), float(x2), float(y2), float(rotation)
 
             except Exception:
                 logger.error("SVG Image argument error, syntax:")
