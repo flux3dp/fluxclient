30379e0201f73ddf1f54678759226c659ccac51f
Yen <yen@flux3dp.com>
2015-10-12 14:44:04 +0800

    new element for scan problem

--- fluxclient/hw_profile/__init__.py
+++ fluxclient/hw_profile/__init__.py
@@ -1,6 +1,10 @@
 HW_PROFILE = {
     "model-1": {
         "radius": 17,
-        'scan_full_len': 360.,
+        'scan_full_len': 115.2,
+        'step_setting': {400: (3, 0.864), 800: (7, 1.008)}
+        # {N: (a, step_len)}
+        # find min a that gcd(a, N) == 1 and scan_full_len / N * a > min_len_that_step_motor_can_move
+        # min_len_that_step_motor_can_move = 0.8
     },
 }
