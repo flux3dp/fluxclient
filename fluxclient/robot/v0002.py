1ee414699f512f734f57befdd0439d86f59699be
Yagami Cerberus <yagami-cerberus@users.noreply.github.com>
2016-03-25 20:02:43 +0800

    Camera: Modify camera interface for ghost

--- fluxclient/robot/v0002.py
+++ fluxclient/robot/v0002.py
@@ -619,37 +619,43 @@ class FluxRobot(ProtocolV2):
 class FluxCamera(ProtocolV2):
     __running__ = False
     __buffer__ = b""
+    __image_length__ = 0
+
+    def feed(self, image_callback):
+        b = self.sock.recv(4096)
+        if b:
+            self.__buffer__ += b
+        else:
+            self.close()
+            raise RuntimeError("Broken pipe")
+        self.unpack_image(image_callback)
 
     def capture(self, image_callback):
         self.__running__ = True
 
-        length = 0
         while self.__running__:
             rl = select((self.sock, ), (), (), 0.05)[0]
             if rl:
-                b = rl[0].recv(4096)
-                if b:
-                    self.__buffer__ += b
-                else:
-                    raise RuntimeError("Broken pipe")
-            else:
-                continue
+                self.feed(image_callback)
 
-            while True:
-                if length:
-                    if len(self.__buffer__) >= length:
-                        image = self.__buffer__[4:length]
-                        self.__buffer__ = self.__buffer__[length:]
-                        length = 0
-                        image_callback(self, image)
-                    else:
-                        break
+    def unpack_image(self, image_callback):
+        length = self.__image_length__
+        while True:
+            if length:
+                if len(self.__buffer__) >= length:
+                    image = self.__buffer__[4:length]
+                    self.__buffer__ = self.__buffer__[length:]
+                    length = 0
+                    image_callback(self, image)
                 else:
-                    if len(self.__buffer__) >= 4:
-                        length = struct.unpack("<I",
-                                               self.__buffer__[:4])[0] + 4
-                    else:
-                        break
+                    break
+            else:
+                if len(self.__buffer__) >= 4:
+                    length = struct.unpack("<I",
+                                           self.__buffer__[:4])[0] + 4
+                else:
+                    break
+        self.__image_length__ = length
 
     def abort(self):
         self.__running__ = False
