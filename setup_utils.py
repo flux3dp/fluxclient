871970002cd83f9a83ae98529c89d7f212509a05
Yen <yen@flux3dp.com>
2016-04-19 18:01:08 +0800

    same as 6e205fe598207a8459587213e29692432010bb5a

--- setup_utils.py
+++ setup_utils.py
@@ -212,7 +212,7 @@ def create_pcl_extentions():
         'fluxclient.printer._printer',
         sources=[
             "src/printer/printer_module.cpp",
-            "src/printer/tree_support.cpp",
+            # "src/printer/tree_support.cpp",
             "src/printer/printer.pyx"],
         language="c++",
         extra_compile_args=extra_compile_args,
