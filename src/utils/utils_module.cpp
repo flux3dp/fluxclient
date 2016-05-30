cd3c2bab20c21edccc51caaf4021fb0be38a1a51
Yen <yen@flux3dp.com>
2016-05-30 17:21:40 +0800

    use %g to shorten the json string

--- src/utils/utils_module.cpp
+++ src/utils/utils_module.cpp
@@ -5,7 +5,7 @@ std::string path_to_js(std::vector< std::vector< std::vector<float> > > path){
   for (size_t layer = 0; layer < path.size(); layer += 1){
     c_string += "[";
       for (size_t point = 0; point < path[layer].size(); point += 1){
-        sprintf(buf, "[%.2f,%.2f,%.2f,%d]", path[layer][point][0], path[layer][point][1], path[layer][point][2], (int)path[layer][point][3]);
+        sprintf(buf, "[%.2g,%.2g,%.2g,%d]", path[layer][point][0], path[layer][point][1], path[layer][point][2], (int)path[layer][point][3]);
         c_string += buf;
         c_string += ",";
       }
