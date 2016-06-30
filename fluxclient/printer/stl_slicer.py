8b27f9cd00537aba8b55e6e906dff0b856e73d66
Yen <yen@flux3dp.com>
2016-06-30 11:53:30 +0800

    change to error code

--- fluxclient/printer/stl_slicer.py
+++ fluxclient/printer/stl_slicer.py
@@ -277,7 +277,7 @@ class StlSlicer(object):
         # p2 = subprocess.Popen(['osascript', pkg_resources.resource_filename("fluxclient", "printer/hide.AppleScript")], stderr=subprocess.STDOUT, stdout=subprocess.PIPE, universal_newlines=True)
         progress = 0.2
         slic3r_error = False
-        slic3r_out = ''
+        slic3r_out = [None, None]
         while subp.poll() is None:
             line = subp.stdout.readline().strip()
             # subp.stdout.read()
@@ -290,7 +290,7 @@ class StlSlicer(object):
                     child_pipe.append('{"slice_status": "computing", "message": "%s", "percentage": %.2f}' % ((line.rstrip())[3:], progress))
                 elif "Unable to close this loop" in line:
                     slic3r_error = True
-                slic3r_out = line
+                slic3r_out = [5, line]  # errorcode 5
         if subp.poll() != 0:
             fail_flag = True
 
@@ -333,7 +333,7 @@ class StlSlicer(object):
 
                 if float(m_GcodeToFcode.md['MAX_R']) >= HW_PROFILE['model-1']['radius']:
                     fail_flag = True
-                    slic3r_out = "gcode area too big"
+                    slic3r_out = [6, "gcode area too big"]  # errorcode 6
 
                 del m_GcodeToFcode
 
@@ -413,7 +413,7 @@ class StlSlicer(object):
                         self.output = None
                         self.metadata = None
                         self.path = None
-                        m = '{"slice_status": "error", "error": "%s"}' % message[1]
+                        m = '{"slice_status": "error", "error": "%d", "info": "%s"}' % (message[1][0], message[1][1])
                     ret.append(m)
         return ret
 
@@ -689,7 +689,7 @@ class StlSlicerCura(StlSlicer):
             # p2 = subprocess.Popen(['osascript', pkg_resources.resource_filename("fluxclient", "printer/hide.AppleScript")], stderr=subprocess.STDOUT, stdout=subprocess.PIPE, universal_newlines=True)
             progress = 0.2
             slic3r_error = False
-            slic3r_out = ''
+            slic3r_out = [None, None]
             while subp.poll() is None:
                 chunck = subp.stdout.readline()
                 for line in chunck.split('\n'):
@@ -701,13 +701,13 @@ class StlSlicerCura(StlSlicer):
                             child_pipe.append('{"slice_status": "computing", "message": "%s", "percentage": %.2f}' % (line, progress))
                         elif "Unable to close this loop" in line:
                             slic3r_error = True
-                        slic3r_out = line
+                        slic3r_out = [5, line]  # errorcode 5
                     # break
             if subp.poll() != 0:
                 fail_flag = True
         except:
             fail_flag = True
-            slic3r_out = 'CuraEngine fail'
+            slic3r_out = [5, 'CuraEngine fail']  # errorcode 5
 
         if config['flux_raft'] == '1':
             m_preprocessor = Raft()
@@ -754,7 +754,7 @@ class StlSlicerCura(StlSlicer):
 
                 if float(m_GcodeToFcode.md['MAX_R']) >= HW_PROFILE['model-1']['radius']:
                     fail_flag = True
-                    slic3r_out = "gcode area too big"
+                    slic3r_out = [6, "gcode area too big"]  # errorcode 6
 
                 del m_GcodeToFcode
 
@@ -909,6 +909,14 @@ class StlSlicerCura(StlSlicer):
 
         # special function for cura's setting file
         # replace '\\n' by '\n', add two lines of '""" indicating multiple lines of settings
+        # in slic3r's setting file:
+        # startCode=aaa\nbbb\nccc
+        #
+        # in cura's setting file:
+        #   startCode="""
+        #   aaa
+        #   bbb
+        #   """
         add_multi_line = lambda x: '"""\n' + x.replace('\\n', '\n') + '\n"""\n'
         new_content['startCode'] = add_multi_line(new_content['startCode'])
         new_content['endCode'] = add_multi_line(new_content['endCode'])
