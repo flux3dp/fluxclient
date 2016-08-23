ee9e757c6dbb42eb96209df953e16278d8c694a2
simonxeko <simonxeko@gmail.com>
2016-08-24 00:03:26 +0800

    Refactor flux_floor to cut_bottom in init.py

--- fluxclient/printer/__init__.py
+++ fluxclient/printer/__init__.py
@@ -218,7 +218,7 @@ z_offset = -1
 flux_refill_empty = 0
 flux_first_layer = 0
 flux_raft = 0
-flux_floor = -1
+cut_bottom = -1
 detect_filament_runout = 1
 detect_head_shake = 1
 detect_head_tilt = 1
@@ -358,7 +358,7 @@ ini_constraint = {
     'flux_refill_empty': [binary],
     'flux_first_layer': [binary],
     'flux_raft': [binary],
-    'flux_floor': [float_range, -1, 240],
+    'cut_bottom': [float_range, -1, 240],
     'detect_filament_runout': [binary],
     'detect_head_shake': [binary],
     'detect_head_tilt': [binary],
