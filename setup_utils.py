5af78ab6a57d2a5f87ea2f560ffa3b029c46f8cd
simonxeko <simonxeko@gmail.com>
2017-03-18 18:25:33 +0800

    locate_includes not working with boost and flann

--- setup_utils.py
+++ setup_utils.py
@@ -151,7 +151,7 @@ def create_pcl_extentions():
 
     try:
         if is_posix():
-            include_dirs += [locate_includes("eigen3")]
+            include_dirs += [locate_includes("eigen3"), '/usr/local/Cellar/boost/1.62.0/include', '/usr/local/Cellar/flann/1.8.4/include']
             libraries += ["pcl_common", "pcl_octree", "pcl_io", "pcl_kdtree",
                           "pcl_search", "pcl_sample_consensus", "pcl_filters",
                           "pcl_features", "pcl_segmentation", "pcl_surface",
